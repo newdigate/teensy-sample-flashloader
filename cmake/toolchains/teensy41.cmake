96d106fc4ce1e456d6ae389bc8141c90cf2f52b8
Newdigate <nicnewdigate@gmail.com>
2023-11-26 12:24:24 +0000

    correct cmake compiler path to v10

--- cmake/toolchains/teensy41.cmake
+++ cmake/toolchains/teensy41.cmake
@@ -2,7 +2,7 @@ set(TEENSY_VERSION 41 CACHE STRING "Set to the Teensy version corresponding to y
 set(CPU_CORE_SPEED 600000000 CACHE STRING "Set to 24000000, 48000000, 72000000 or 96000000 to set CPU core speed" FORCE) # Derived variables
 set(CMAKE_EXE_LINKER_FLAGS "--specs=nano.specs" CACHE INTERNAL "")
 #teensy compiler options
-set(COMPILERPATH "/opt/gcc-arm-none-eabi-9-2019-q4-major/bin/")
+set(COMPILERPATH "/opt/gcc-arm-none-eabi-10.3-2021.10/bin/")
 #set(COMPILERPATH "/Applications/ARM/bin/")
 set(DEPSPATH "/home/runner/work/teensy-sample-flashloader/teensy-sample-flashloader/deps")
 #set(DEPSPATH "/Users/nicholasnewdigate/Development/github/newdigate/temp_dep")
