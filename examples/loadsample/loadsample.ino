ceb55b033f958b08b1854859fa6044c8b0fb08db
Newdigate <nicnewdigate@gmail.com>
2023-12-07 21:45:07 +0000

    untested, work in progress!

--- examples/loadsample/loadsample.ino
+++ examples/loadsample/loadsample.ino
@@ -11,14 +11,14 @@
 #include <SerialFlash.h>
 
 // GUItool: begin automatically generated code
-AudioPlayArrayResmp      rraw_a7;        //xy=167,520
-AudioPlayArrayResmp      rraw_a6;        //xy=168,489
-AudioPlayArrayResmp      rraw_a8;        //xy=168,551
-AudioPlayArrayResmp      rraw_a5;        //xy=169,456
-AudioPlayArrayResmp      rraw_a3;        //xy=172,356
-AudioPlayArrayResmp      rraw_a4;        //xy=172,387
-AudioPlayArrayResmp      rraw_a2;        //xy=177,317
-AudioPlayArrayResmp      rraw_a1;        //xy=181,282
+AudioPlayArrayResmp      voice7;        //xy=167,520
+AudioPlayArrayResmp      voice6;        //xy=168,489
+AudioPlayArrayResmp      voice8;        //xy=168,551
+AudioPlayArrayResmp      voice5;        //xy=169,456
+AudioPlayArrayResmp      voice3;        //xy=172,356
+AudioPlayArrayResmp      voice4;        //xy=172,387
+AudioPlayArrayResmp      voice2;        //xy=177,317
+AudioPlayArrayResmp      voice1;        //xy=181,282
 AudioMixer4              mxr_fr1_left;         //xy=360,295
 AudioMixer4              mxr_fr1_right;         //xy=360,363
 AudioMixer4              mxr_fr2_right;         //xy=361,534
@@ -26,22 +26,22 @@ AudioMixer4              mxr_fr2_left;         //xy=364,463
 AudioMixer4              mxr_back_left;         //xy=587,394
 AudioMixer4              mxr_back_right;         //xy=589,459
 AudioOutputI2S           i2s1;           //xy=757,443
-AudioConnection          patchCord1(rraw_a7, 0, mxr_fr2_left, 2);
-AudioConnection          patchCord2(rraw_a7, 1, mxr_fr2_right, 2);
-AudioConnection          patchCord3(rraw_a6, 0, mxr_fr2_left, 1);
-AudioConnection          patchCord4(rraw_a6, 1, mxr_fr2_right, 1);
-AudioConnection          patchCord5(rraw_a8, 0, mxr_fr2_left, 3);
-AudioConnection          patchCord6(rraw_a8, 1, mxr_fr2_right, 3);
-AudioConnection          patchCord7(rraw_a5, 0, mxr_fr2_left, 0);
-AudioConnection          patchCord8(rraw_a5, 1, mxr_fr2_right, 0);
-AudioConnection          patchCord9(rraw_a3, 0, mxr_fr1_left, 2);
-AudioConnection          patchCord10(rraw_a3, 1, mxr_fr1_right, 2);
-AudioConnection          patchCord11(rraw_a4, 0, mxr_fr1_left, 3);
-AudioConnection          patchCord12(rraw_a4, 1, mxr_fr1_right, 3);
-AudioConnection          patchCord13(rraw_a2, 0, mxr_fr1_left, 1);
-AudioConnection          patchCord14(rraw_a2, 1, mxr_fr1_right, 1);
-AudioConnection          patchCord15(rraw_a1, 0, mxr_fr1_left, 0);
-AudioConnection          patchCord16(rraw_a1, 1, mxr_fr1_right, 0);
+AudioConnection          patchCord1(voice7, 0, mxr_fr2_left, 2);
+AudioConnection          patchCord2(voice7, 1, mxr_fr2_right, 2);
+AudioConnection          patchCord3(voice6, 0, mxr_fr2_left, 1);
+AudioConnection          patchCord4(voice6, 1, mxr_fr2_right, 1);
+AudioConnection          patchCord5(voice8, 0, mxr_fr2_left, 3);
+AudioConnection          patchCord6(voice8, 1, mxr_fr2_right, 3);
+AudioConnection          patchCord7(voice5, 0, mxr_fr2_left, 0);
+AudioConnection          patchCord8(voice5, 1, mxr_fr2_right, 0);
+AudioConnection          patchCord9(voice3, 0, mxr_fr1_left, 2);
+AudioConnection          patchCord10(voice3, 1, mxr_fr1_right, 2);
+AudioConnection          patchCord11(voice4, 0, mxr_fr1_left, 3);
+AudioConnection          patchCord12(voice4, 1, mxr_fr1_right, 3);
+AudioConnection          patchCord13(voice2, 0, mxr_fr1_left, 1);
+AudioConnection          patchCord14(voice2, 1, mxr_fr1_right, 1);
+AudioConnection          patchCord15(voice1, 0, mxr_fr1_left, 0);
+AudioConnection          patchCord16(voice1, 1, mxr_fr1_right, 0);
 AudioConnection          patchCord17(mxr_fr1_left, 0, mxr_back_left, 0);
 AudioConnection          patchCord18(mxr_fr1_right, 0, mxr_back_right, 0);
 AudioConnection          patchCord19(mxr_fr2_right, 0, mxr_back_right, 1);
@@ -51,33 +51,31 @@ AudioConnection          patchCord22(mxr_back_right, 0, i2s1, 1);
 AudioControlSGTL5000     sgtl5000_1;     //xy=371,652
 // GUItool: end automatically generated code
 
-
 newdigate::audiosample  *samples[2][4];
 AudioPlayArrayResmp      *voices[2][4] =  {
-        { &rraw_a1, &rraw_a2, &rraw_a3, &rraw_a4 },
-        { &rraw_a5, &rraw_a6, &rraw_a7, &rraw_a8 } };
+        { &voice1, &voice2, &voice3, &voice4 },
+        { &voice5, &voice6, &voice7, &voice8 } };
 
 newdigate::flashloader loader( 16 * 1024);
-char * fileNames[2][4] =  {
-        {"BASS1.RAW", "BASS2.RAW", "BASS3.RAW", "BASS4.RAW" },
-        {"BASS5.RAW", "BASS6.RAW", "BASS7.RAW", "BASS8.RAW" } };
+const char * fileNames[2][4] =  {
+        {"loop1.raw", "loop3.raw", "loop5.raw", "loop7.raw" },
+        {"loop2.raw", "loop4.raw", "loop6.raw", "loop8.raw" } };
 
-File file;
+File asyncLoadFile;
 int numLoaded = 0;
 bool asyncOpening = true, asyncChanging = false, isPlaying = false;
-uint32_t currentWriteHeap = 0, currentReadHeap = 1;
-uint32_t patternStartTime;
+uint32_t currentWriteHeap = 0, currentReadHeap = 1, patternStartTime = 0;
+
 const double tempo = 120.0; //Bpm
 const double durationOfOneBeatInMilliseconds = 1000.0 * (60.0 / tempo);
 const uint32_t durationOf32BeatsInMilliseconds = durationOfOneBeatInMilliseconds * 32.0;
 
 void setup() {
     Serial.begin(9600);
-    AudioMemory(20);
+    AudioMemory(32);
+
     sgtl5000_1.enable();
     sgtl5000_1.volume(0.5f, 0.5f);
-
-    rraw_a1.enableInterpolation(true);
 
     Serial.print("Initializing SD card...");
     while (!SD.begin(BUILTIN_SDCARD)) {
@@ -85,7 +83,6 @@ void setup() {
         delay(1000);
     }
     Serial.println("initialization done.");
-
 }
 
 inline void heap_switch() {
@@ -101,8 +98,8 @@ void loop() {
     if (numLoaded < 4) {
         if (asyncOpening) {
             const auto filename = fileNames[currentWriteHeap][numLoaded];
-            file = SD.open(filename);
-            samples[currentWriteHeap][numLoaded] = loader.beginAsyncLoad(file);
+            asyncLoadFile = SD.open(filename);
+            samples[currentWriteHeap][numLoaded] = loader.beginAsyncLoad(asyncLoadFile);
             if (!samples[currentWriteHeap][numLoaded]) {
                 Serial.printf("abort!!! can not find '%s'...(heap:%d, index:%d)\n", filename, currentWriteHeap, numLoaded);
                 delay(1000);
@@ -113,7 +110,7 @@ void loop() {
         } else if (loader.continueAsyncLoadPartial()) {
             Serial.printf("loading complete...  (heap:%d, index:%d)\n", currentWriteHeap, numLoaded);
             numLoaded++;
-            file.close();
+            asyncLoadFile.close();
             asyncOpening = true;
         }
     } else if (!isPlaying) {
@@ -153,6 +150,9 @@ void loop() {
             if (!voice->isPlaying()) {
                 voice->stop();
             }
+        }
+        if (numLoaded < 4) {
+            Serial.println("WARN: not all the samples managed to load in time!");
         }
         Serial.println("Audio loaded, playback enabled!");
         loader.toggle_afterNewPatternStarts();
