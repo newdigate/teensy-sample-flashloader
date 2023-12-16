32f48ef2e987179d1dcb039d503605acd5aae31c
Newdigate <nicnewdigate@gmail.com>
2023-12-16 16:41:41 +0000

    cmake toolchain correction

--- cmake/toolchains/teensy41.cmake
+++ cmake/toolchains/teensy41.cmake
@@ -2,10 +2,10 @@ set(TEENSY_VERSION 41 CACHE STRING "Set to the Teensy version corresponding to y
 set(CPU_CORE_SPEED 600000000 CACHE STRING "Set to 24000000, 48000000, 72000000 or 96000000 to set CPU core speed" FORCE) # Derived variables
 set(CMAKE_EXE_LINKER_FLAGS "--specs=nano.specs" CACHE INTERNAL "")
 #teensy compiler options
-#set(COMPILERPATH "/opt/gcc-arm-none-eabi-10.3-2021.10/bin/")
-set(COMPILERPATH "/Applications/ARM_10/bin/")
-#set(DEPSPATH "/home/runner/work/teensy-sample-flashloader/teensy-sample-flashloader/deps")
-set(DEPSPATH "/Users/nicholasnewdigate/Development/github/newdigate/temp_dep")
+set(COMPILERPATH "/opt/gcc-arm-none-eabi-10.3-2021.10/bin/")
+#set(COMPILERPATH "/Applications/ARM_10/bin/")
+set(DEPSPATH "/home/runner/work/teensy-sample-flashloader/teensy-sample-flashloader/deps")
+#set(DEPSPATH "/Users/nicholasnewdigate/Development/github/newdigate/temp_dep")
 set(COREPATH "${DEPSPATH}/cores/teensy4/")
 
 find_package(teensy_cmake_macros)
