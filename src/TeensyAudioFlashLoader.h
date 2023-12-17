c14d7a2ca6f6bc01409311f6dd76f2af08718ee2
Newdigate <nicnewdigate@gmail.com>
2023-12-17 10:44:45 +0000

    improvement to main header file to include audiosample and flashloader

--- src/TeensyAudioFlashLoader.h
+++ src/TeensyAudioFlashLoader.h
@@ -1,6 +1,7 @@
 #ifndef TEENSY_AUDIO_FLASHLOADER_TEENSYAUDIOFLASHLOADER_H
 #define TEENSY_AUDIO_FLASHLOADER_TEENSYAUDIOFLASHLOADER_H
 
+#include "flashloader.h"
 #include "dualheapasyncflashloader.h"
-
+#include "audiosample.h"
 #endif
